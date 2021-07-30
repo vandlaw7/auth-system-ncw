7d14233edfe90603436510f9f83d5500a3070dbc
Changwoo Nam <vandlaw@naver.com>
2021-07-30 16:32:49 +0900

    router.push('/') after wallet remove

--- components/Wallet.tsx
+++ components/Wallet.tsx
@@ -13,6 +13,7 @@ async function removeWallet(id: String): Promise<void> {
     const response = await fetch(`https://auth-system-ncw.vercel.app/api/wallet/${id}`, {
         method: 'DELETE',
     });
+    Router.push('/');
     const response_json = await response.json()
     // 마지막 남은 계좌를 삭제하면 해당 유저가 삭제되고 자동으로 로그아웃됨.
     if (response_json.user_delete){
--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,9 +1,12 @@
 import { PrismaClient } from "@prisma/client";
 
 // Prevent multiple instances of Prisma Client in development
-declare const global: NodeJS.Global & { prisma?: PrismaClient };
+declare const global: NodeJS.Global
+    & { prisma?: PrismaClient };
 
-const prisma = global.prisma || new PrismaClient();
-if (process.env.NODE_ENV === "development") global.prisma = prisma;
+const prisma = global.prisma
+    || new PrismaClient();
+if (process.env.NODE_ENV === "development")
+    global.prisma = prisma;
 
 export default prisma;
